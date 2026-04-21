76b99e69847393f7fda79d44c5f595d7b552f615
MAOYoshida-0109 <nk250112@nks.iput.ac.jp>
2026-04-21 15:19:57 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+//ルート１：トップページ
+app.get('/',(req, res) => {
+    res.send('トップページです');
+});
+
+//ルート２：自己紹介ページ
+app.get('/about',(req, res) => {
+    res.send('自己紹介ページです');
+});
+
+//ルート３：現在時刻を返す
+app.get('/time',(req, res) => {
+    const now = new Date().toLocaleString('ja-JP');
+    res.send('現在時刻:' + now);
+});
+
+app.listen(3000, () => {
+    console.log('サーバーが起動しました: http://localhost:3000');
+});
+
