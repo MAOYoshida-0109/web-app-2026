152f1dbd4515b54f8d08019e8982044edc6b020f
MAOYoshida-0109 <nk250112@nks.iput.ac.jp>
2026-05-19 15:52:54 +0900

    POSTエンドポイントとAPI仕様を追加

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.static('public'));
+
+
 //ルート１：トップページ
 app.get('/',(req, res) => {
     res.send('トップページです');
@@ -15,6 +19,30 @@ app.get('/about',(req, res) => {
 app.get('/time',(req, res) => {
     const now = new Date().toLocaleString('ja-JP');
     res.send('現在時刻:' + now);
+});
+
+app.get('/status', (req, res) => {
+    res.json({ status: 'ok', message: 'サーバーが動いています' });
+});
+
+app.get('/status', (req, res) => {
+    res.json({ status: 'ok' , message: 'APIが動いています' });
+});
+
+
+const messages = [];
+
+// GET:メッセージ一覧を取得
+app.get('/api/messages',(req, res) => {
+    res.json(messages);
+});
+
+// POST:メッセージを追加
+app.post('/api/messages',(req, res) => {
+    const {username, text } = req.body;
+    const newMessage = { id: messages.length + 1, username, text };
+    messages.push(newMessage);
+    res.json(newMessage);
 });
 
 app.listen(3000, () => {
