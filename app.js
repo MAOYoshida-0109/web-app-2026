b65b4297b1253a86d0f4c6906269b56b14560a5c
MAOYoshida-0109 <nk250112@nks.iput.ac.jp>
2026-06-01 19:19:01 +0900

    ポート番号を環境変数から読むように変更

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ app.post('/api/tasks', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('サーバーが起動しました: http://localhost:3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`サーバーが起動しました: http://localhost:${process.env.PORT || 3000}`);
 });
